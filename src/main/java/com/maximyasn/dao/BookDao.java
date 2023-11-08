48652b993e0c5aa271a3bb644c65edf297aa956e
maximyasn <maximyasniy11@gmail.com>
2023-11-08 18:41:49 +0300

    Rebase app to spring data jpa

--- src/main/java/com/maximyasn/dao/BookDao.java
+++ src/main/java/com/maximyasn/dao/BookDao.java
@@ -1,14 +1,14 @@
 //package com.maximyasn.dao;
 
-import com.maximyasn.entity.Book;
-import com.maximyasn.entity.Person;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Component;
-
-import java.util.List;
-import java.util.Optional;
+//import com.maximyasn.entity.Book;
+//import com.maximyasn.entity.Person;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.jdbc.core.BeanPropertyRowMapper;
+//import org.springframework.jdbc.core.JdbcTemplate;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.List;
+//import java.util.Optional;
 
 //@Component
 //public class BookDao {
